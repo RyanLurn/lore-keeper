528809f05fc266ca9129ba9281742527943a8fa9
Ryan Luong <lumintriet@gmail.com>
2025-08-21 22:01:48 +0700

    Add dark mode and toast.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { toast } from "sonner";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <ModeToggle className="fixed top-3 right-3" />
+      <Button onClick={() => toast.info("Hello")}>Click me</Button>
     </div>
   );
 }
