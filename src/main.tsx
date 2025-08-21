a8d7b40499065aab190bee7eb32d9d1a26eeb329
Ryan Luong <lumintriet@gmail.com>
2025-08-21 22:16:57 +0700

    Add React Scan.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
+import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app";
 import { Providers } from "@/components/providers";
 import "@/index.css";
+
+scan({
+  enabled: true
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
