9550ee63af7a2137efa75209d696d27ea4b3e839
Ryan Luong <lumintriet@gmail.com>
2025-08-21 22:25:28 +0700

    Setup Convex.

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "**/convex/_generated/"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
