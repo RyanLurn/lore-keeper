2e6568e506db95d69ae2b4b3e69cb7dc9501bd9c
Ryan Luong <lumintriet@gmail.com>
2025-08-22 20:40:23 +0700

    Message avatar component.

---
+++ src/components/message/avatar.tsx
@@ -0,0 +1,29 @@
+import { memo } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { capitalizeFirstLetter, cn } from "@/lib/utils";
+
+const MessageAvatar = memo(function MessageAvatar({
+  className,
+  role,
+  name,
+  src
+}: {
+  className?: string;
+  role: "system" | "user" | "assistant";
+  name?: string;
+  src?: string;
+}) {
+  const avatarName = name ?? capitalizeFirstLetter(role);
+  return (
+    <Avatar className={cn("size-8 ring ring-border", className)}>
+      <AvatarImage
+        alt={`${avatarName}'s image`}
+        className="mt-0 mb-0"
+        src={src}
+      />
+      <AvatarFallback>{avatarName.slice(0, 2).toUpperCase()}</AvatarFallback>
+    </Avatar>
+  );
+});
+
+export { MessageAvatar };
