8b86b56989ee3ce23887481dfa0b9977ea2f7435
Ryan Luong <lumintriet@gmail.com>
2025-08-22 20:44:44 +0700

    Message content components.

---
+++ src/components/message/content/index.tsx
@@ -0,0 +1,34 @@
+import { memo } from "react";
+import type { Doc } from "backend/_generated/dataModel";
+import { TextPart } from "@/components/message/content/text-part";
+import { cn } from "@/lib/utils";
+
+const MessageContent = memo(function MessageContent({
+  _id,
+  parts,
+  className
+}: Pick<Doc<"messages">, "_id" | "parts"> & {
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-2 overflow-hidden rounded-lg px-4 py-3 text-sm text-foreground",
+        "group-[.is-user]:bg-primary group-[.is-user]:text-primary-foreground",
+        "group-[.is-assistant]:bg-secondary group-[.is-assistant]:text-foreground",
+        className
+      )}
+    >
+      {parts.map((part, i) => {
+        switch (part.type) {
+          case "text":
+            return <TextPart key={`${_id}-${i}`} text={part.text} />;
+          default:
+            return null;
+        }
+      })}
+    </div>
+  );
+});
+
+export { MessageContent };
---
+++ src/components/message/content/text-part.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { markdownComponents } from "@/components/typography/markdown";
+import { cn } from "@/lib/utils";
+
+const TextPart = memo(function TextPart({
+  className,
+  text
+}: {
+  className?: string;
+  text: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
+        className
+      )}
+    >
+      <Markdown components={markdownComponents} remarkPlugins={[remarkGfm]}>
+        {text}
+      </Markdown>
+    </div>
+  );
+});
+
+export { TextPart };
